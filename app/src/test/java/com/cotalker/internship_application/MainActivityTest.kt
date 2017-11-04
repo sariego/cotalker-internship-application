f3c095ac859dd32cfcc9a9cf13c61ef6ec0c3120
Pedro Sariego <pedrosariego@gmail.com>
2017-11-03 22:10:44 -0300

    added simple test

--- app/src/test/java/com/cotalker/internship_application/MainActivityTest.kt
+++ app/src/test/java/com/cotalker/internship_application/MainActivityTest.kt
@@ -3,13 +3,24 @@ package com.cotalker.internship_application
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Created by sariego on 11/3/17.
  */
 class MainActivityTest {
+
+    lateinit var activity: MainActivity
+
+    @Before
+    fun setup() {
+        activity = MainActivity()
+    }
+
     @Test
     fun replaceAsterisks() {
+        assertEquals(listOf("1001000", "1001010", "1011000", "1011010"), activity.replaceAsterisks("10*10*0"))
+        assertEquals(listOf("00", "10"), activity.replaceAsterisks("*0"))
     }
 
 }
